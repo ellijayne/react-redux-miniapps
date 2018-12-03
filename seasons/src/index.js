43314b03096adc833f121d9c0919ab21117679f1
ejwillard <ejwillard@squiz.net>
2018-12-03 16:10:32 +1100

    reworked functional component into class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        //fnction callbacks - this is the success callback, it gets called when our request was successful. the second one is the 'failure callback'
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div>Hi There!</div>
-};
+//below is the old functional version of the class component below.
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         //fnction callbacks - this is the success callback, it gets called when our request was successful. the second one is the 'failure callback'
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+//     return <div>Latitude: </div>
+// };
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+        
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>
+    }
+}
 
 ReactDOM.render(
     <App />, 
